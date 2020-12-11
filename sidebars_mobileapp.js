6268a1b5b88f38d85b7bb227747571b481ae2f6f
Daniel Dimitrov <daniel@compojoom.com>
2020-12-11 14:43:58 +0100

    Add storybook docs

--- sidebars_mobileapp.js
+++ sidebars_mobileapp.js
@@ -4,23 +4,32 @@ module.exports = {
       type: "category",
       label: "The Basics",
       collapsed: false,
-      items: ["introduction", 'getting_started']
+      items: ["introduction", "getting_started"],
     },
     {
       type: "category",
       label: "Customizations",
       collapsed: false,
-      items: ["customizations/custom_icons", "customizations/clientlib", "customizations/state"]
+      items: [
+        "customizations/custom_icons",
+        "customizations/clientlib",
+        "customizations/state",
+        "customizations/storybook"
+      ],
     },
     {
       type: "category",
       collapsed: false,
       label: "Known issues",
-      items: ["known_issues/javascript", "known_issues/android", "known_issues/ios"]
+      items: [
+        "known_issues/javascript",
+        "known_issues/android",
+        "known_issues/ios",
+      ],
     },
     {
       type: "doc",
-      id: "e2e_testing"
-    }
-  ]
+      id: "e2e_testing",
+    },
+  ],
 }
